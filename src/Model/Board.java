408470880c2cca4267719f0d3e97336d5c56e0fd
Azriel <44496680+azrielortega@users.noreply.github.com>
2020-09-04 22:30:54 +0800

    Added OrageSpace and Space

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,5 +1,12 @@
 package Model;
+import java.util.ArrayList;
 
 public class Board {
-
+	
+	private static final int SPACES = 84;
+	private ArrayList <Space> spaces;
+	
+	public Board () {
+		spaces = new ArrayList <>();
+	}
 }

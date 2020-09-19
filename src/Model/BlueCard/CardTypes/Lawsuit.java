6c853fa53c9acdb7090997110d0e5e69db7a74ac
dychosenone <50646953+dychosenone@users.noreply.github.com>
2020-09-19 11:23:45 +0800

    changed config

--- src/Model/BlueCard/CardTypes/Lawsuit.java
+++ src/Model/BlueCard/CardTypes/Lawsuit.java
@@ -1,5 +1,29 @@
 package Model.BlueCard.CardTypes;
 
-public class Lawsuit {
+import Model.BlueCard.BlueCard;
 
+public class Lawsuit extends BlueCard{
+	
+	private int amount;
+	
+	public Lawsuit() {
+		
+		super("Lawsuit", "Lawyer");
+		generateAmount();
+	}
+	
+	public int generateAmount () {
+		
+		int total;
+		
+		do{
+			total = (int) (Math.random() * (10 - 1 + 1) + 1) * 10000;
+		} while (total >= 50000 || total <= 150000);
+		
+		return total;
+		
+	}
+		
+	}
+	
 }

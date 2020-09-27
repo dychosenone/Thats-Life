1984a4cb53c9b8660520a9b0cba5db786b08c131
dychosenone <50646953+dychosenone@users.noreply.github.com>
2020-09-27 22:48:46 +0800

    spaces javadocs

--- src/Model/MagentaSpace.java
+++ src/Model/MagentaSpace.java
@@ -3,13 +3,25 @@ package Model;
 public class MagentaSpace extends Space {
 	
 	private String magentaType;
-	
+
+	/**
+	 * The magenta space extends the Space class. The magenta space has a magentaType which is the type of magentaSpace.
+	 * @param number board position number
+	 * @param spaceName the name of space
+	 * @param isChoosePath if has choose path
+	 * @param pathJumpString the pathJump if any (use -1 if none)
+	 * @param magentaType the type of magenta
+	 */
 	public MagentaSpace (int number, String spaceName, boolean isChoosePath, int pathJumpString, String magentaType) {
 		super (number, spaceName, isChoosePath, pathJumpString);
 		this.magentaType = magentaType;
 
 	}
 
+	/**
+	 * Gets the type of magenta space and returns it.
+	 * @return Type of Magenta Space
+	 */
 	public String getMagentaType () {
 		return this.magentaType;
 	}
--- src/Model/Space.java
+++ src/Model/Space.java
@@ -6,9 +6,16 @@ public class 	Space {
 		private String spaceName;
 		private boolean isChoosePath;
 		private int pathJump;
-	
-	
-	
+
+
+	/**
+	 * Initializes the Space using the Space constructor. The constructor sets the board Position number of the space, the type of space,
+	 * if there is a choosePath, and the pathJump of the space.
+	 * @param number number of the Space (Position)
+	 * @param spaceName Type of Space
+	 * @param isChoosePath If there is a choose path
+	 * @param pathJump pathJump (if any) if none use -1
+	 */
 	public Space (int number, String spaceName, boolean isChoosePath, int pathJump) {
 		this.spaceNumber = number;
 		this.spaceName = spaceName;
@@ -16,14 +23,27 @@ public class 	Space {
 		this.pathJump = pathJump;
 		
 	}
-	
+
+	/**
+	 * Gets the name of the space. (Orange, Green, Blue, etc..)
+	 * @return Name of Space
+	 */
 	public String getName () {
 		return this.spaceName;
 	}
+
+	/**
+	 * Gets the Space Number, or the position in the board.
+	 * @return Space Number / Board Position
+	 */
 	public int getSpaceNumber () {
 		return this.spaceNumber;
 	}
 
+	/**
+	 * Takes the space Jump of the board, if no space Jump, value is -1.
+	 * @return pathJump of Space (if any) returns -1 none.
+	 */
 	public int pathJump () {
 		return this.pathJump;
 	}
